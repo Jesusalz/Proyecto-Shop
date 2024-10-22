128b6e645ed20ce789b1f55923bc3f6b8caea4b5
Jesusalz <jesus.uculture@gmail.com>
2024-10-22 16:50:37 -0300

    Actualizado login

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,15 +1,39 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import ProductSearch from './ProductSearch'; 
+import { useDispatch, useSelector } from 'react-redux';
+import { logout } from '../store/authSlice';
 
 const NavBar = () => {
+  const dispatch = useDispatch();
+  const { user } = useSelector((state) => state.auth);
+
+  const handleLogout = () => {
+    dispatch(logout()); 
+    
+  };
+
   return (
     <nav className="bg-gray-800 p-4 flex justify-between items-center">
       <div className="text-white">
         <Link to="/">Home</Link>
-        {/* Otros enlaces */}
+        
       </div>
-      <ProductSearch /> {/* Agrega el buscador aquí */}
+      <ProductSearch />
+      <div className="text-white">
+        {user ? (
+          <>
+            <span className="mr-4">Hola, {user.name}</span>
+            <button onClick={handleLogout} className="bg-red-500 hover:bg-red-600 text-white py-1 px-4 rounded">
+              Cerrar Sesión
+            </button>
+          </>
+        ) : (
+          <Link to="/login" className="bg-blue-500 hover:bg-blue-600 text-white py-1 px-4 rounded">
+            Iniciar Sesión
+          </Link>
+        )}
+      </div>
     </nav>
   );
 };
